1e3d1e71faf6d62d862cfff9df49dcd58e218a29
Chiragchichi <161250768+Chiragchichi@users.noreply.github.com>
2024-07-10 11:42:33 +0000

    new change

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,32 @@
+// const score = 100
+// console.log(score);
+// const newScore = new Number(300)
+// console.log(newScore);
+
+
+// console.log(newScore.toString().length);
+// console.log(newScore.toFixed(1));
+
+// const otherScore = 23.8475
+// console.log(otherScore.toPrecision(4));
+
+// const scoreOne = 1000000000
+// console.log(scoreOne.toLocaleString('In-en'));
+
+// ++++++++++MATHS++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.8));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4,8,12.16));
+// console.log(Math.max(4,8,12,16));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10+1));
+
+const min=100
+const max=200
+console.log(Math.floor(Math.random() * (max - min + 1))+ min);
+
