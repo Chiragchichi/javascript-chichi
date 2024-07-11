dd591d4938feba5c3140d3f31d2ccc0660e60fa1
Chiragchichi <161250768+Chiragchichi@users.noreply.github.com>
2024-07-11 18:35:01 +0000

    functions done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -9,18 +9,40 @@
 // }
 // intro()
 
-function sum(num1,num2){
-   return num1+num2
-}
-let result=sum(4,6)
-// console.log(result);
+// function sum(num1,num2){
+//    return num1+num2
+// }
+// let result=sum(4,6)
+// // console.log(result);
 
-function loggedIn(username){
-   if(username===undefined){
-      console.log("please enter a name");
-      return 
-   }
-      return `${username} just logged in`
+// function loggedIn(username){
+//    if(username===undefined){
+//       console.log("please enter a name");
+//       return 
+//    }
+//       return `${username} just logged in`
 
+// }
+// console.log(loggedIn());
+
+// function myCart(...num1){
+//    return num1
+// }
+// // console.log(myCart(200,400,600));
+
+// const user={
+//    name:"chirag",
+//    id:"chirsg@gmail"
+// }
+// function mail(anyUser){
+//    console.log(`${anyUser.name} is my name and ${anyUser.id} is my mail`);
+// }
+// mail({
+//    name:"rudraax",
+//    id:"rudi@gmail"
+// })
+const myArray=[100,200,300,400]
+function ArrayAA(usArray){
+   return usArray[1]
 }
-console.log(loggedIn());
+console.log(ArrayAA(myArray));
